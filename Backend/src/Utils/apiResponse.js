396728dc66299321a9a31239a965b7d3baf7fdcb
Jaimin Prajapati <jaiminb251203@gmail.com>
2025-02-27 19:46:51 +0530

    removing giles

--- Backend/src/Utils/apiResponse.js
+++
@@ -1,9 +0,0 @@
-const apiResponse = (res, success, data, message, statusCode = 200) => {
-  return res.status(statusCode).json({
-    success: success,
-    message: message,
-    data: data || null,
-  });
-};
-
-export default apiResponse;
