d24ec08e579dd9c49eeb329897f9f685b683698c
Jaimin Prajapati <jaiminb251203@gmail.com>
2025-05-07 22:01:42 +0530

    Notification model completed

---
+++ backend/src/models/notificationModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["order", "product", "address", "system"],
+      default: "system",
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    relatedOrderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+    relatedProductId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification =
+  mongoose.models.Notification ||
+  mongoose.model("Notification", notificationSchema);
+export default Notification;
